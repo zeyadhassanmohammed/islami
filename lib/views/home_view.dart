0ef211a2a22c1b7baaefff18a76592fda555f8c8
ZEYAD <zeyadelaray1@gmail.com>
2025-01-26 17:19:12 +0200

    finish home screen

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,15 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:islami/views/tabs/hadeth_tab.dart';
+import 'package:islami/views/tabs/quran_tab.dart';
+import 'package:islami/views/tabs/radio_tab.dart';
+import 'package:islami/views/tabs/sebha_tab.dart';
+import 'package:islami/views/tabs/settings_tab.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   @override
   static const String routeName = "homeView";
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+    int selectedIndex = 0;
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("islami"),
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/main_bg.png"),
+              fit: BoxFit.fill)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(
+            "اسلامي",
+            style: GoogleFonts.elMessiri(
+                fontSize: 30, fontWeight: FontWeight.w700),
+          ),
+          centerTitle: true,
+        ),
+
+        body: tabs[selectedIndex],
+
+
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: selectedIndex,
+            onTap: (value) {
+              selectedIndex = value;
+              setState(() {
+
+              });
+            },
+            type: BottomNavigationBarType.shifting,
+            elevation: 0,
+            selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.white,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            items: [
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage("assets/images/quran_icn.png")),
+                  label: "",backgroundColor: Color(0xFFB7935F),),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage("assets/images/hadeth_icn.png")),
+                  label: "",backgroundColor: Color(0xFFB7935F)),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage("assets/images/sebha_icn.png")),
+                  label: "",backgroundColor: Color(0xFFB7935F)),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage("assets/images/radio_icn.png")),
+                  label: "",backgroundColor: Color(0xFFB7935F)),
+              BottomNavigationBarItem(icon: Icon(Icons.settings), label: "",backgroundColor: Color(0xFFB7935F)),
+            ]),
       ),
     );
   }
+  List<Widget>tabs=[
+    QuranTab(),
+    HadethTab(),
+    SebhaTab(),
+    RadioTab(),
+    SettingsTab(),
+  ];
 }
